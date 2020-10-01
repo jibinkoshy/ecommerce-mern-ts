240dab4e0d897cf40828e74b6a4c1d7464d6c1f7
jibinkoshy <jibinkoshy@hotmail.com>
2020-10-01 00:11:50 -0500

    add Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App: React.FC = () => {
-  return <>Hello</>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Ecommerce</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default App;
